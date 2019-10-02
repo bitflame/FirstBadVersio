3ed1c8ad01213acf2ae3d3983fb783d98a63b8d9
Liquid Falme <sansari1@cox.net>
2019-10-02 13:02:51 -0400

    Successfully submited. Added an interface to the project so it will work in a Visual Studio project like LeetCode.

---
+++ FirstBadVersio/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBadVersio
+{
+    class Program : IsBadVersion
+    {
+
+        static void Main(string[] args)
+        {
+            FirstBadVersion(10);
+        }
+        private static int FirstBadVersion(Int32 n)
+        {
+            int start = 0;
+            int end = n;
+            Program p = new Program();
+            while (start + 1 < end)
+            {
+                int mid = start + (end - start) / 2;
+                if (p.IsBadVersion(mid))
+                {
+                    end = mid;
+                }
+                else
+                {
+                    start = mid;
+                }
+            }
+            if (p.IsBadVersion(start))
+            {
+                return start;
+            }
+            return end;
+        }
+
+        public bool IsBadVersion(int n)
+        {
+            if (n <= 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
